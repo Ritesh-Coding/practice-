fd8bcecb3c955e6fbf56e1976d2a1f4e8e387c8b
Ritesh-Jaiswal <riteshjaiswal@esparkbizmail.com>
2024-07-12 19:47:51 +0530

    job-form real

---
+++ real/job_app_form/job_form/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+
+urlpatterns= [
+    path('',views.home,name='home'),
+    path('register/',views.RegistrationForm,name='register'),
+    path('login/',views.LoginForm,name='login'),    
+    path('logout/', views.user_logout, name='logout'),
+    path('job-application-form/', views.EmployeeForm, name='employeeForm'),
+    path('success/', views.success, name='success'),
+    path('userData/<int:id>/',views.userData,name='userData'),
+    path('updateForm/<int:id>',views.updateForm,name='updateForm'),
+    path('deleteForm/<int:id>/',views.deleteForm,name='deleteForm'),
+    path('confirmDelete/<int:id>/',views.confirmDelete,name='confirmDelete'),
+    path('admin-dashboard/',views.adminDashBoard,name='adminDashboard'),
+    path('dashboard',views.dashboard,name="dashboard"),
+    path('api/cities',views.cities,name='cities'),
+    path('update-job-application-form/<int:id>/',views.updateEmployeeForm,name='updateEmployeeForm'),
+
+]
